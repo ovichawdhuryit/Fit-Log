3656c50aa3a65e61e1ec571ecbb6b5dcb861c748
Anik Chawdhury <ovichawdhuryit@gmail.com>
2026-09-25 14:03:55 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import NavbarPage from "./Navbar/page";
+
+export default function Home() {
+  return (
+    <div>
+      <NavbarPage></NavbarPage>
+    </div>
+  );
+}
