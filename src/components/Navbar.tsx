0d95a74a4b7e126114e3980bf70f50f34e84fc93
Anik Chawdhury <ovichawdhuryit@gmail.com>
2026-09-25 19:24:17 +0600

    Polished NavBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,33 +9,35 @@ const navbar = () => {
         <li><a>My Plan </a></li>
     </>
     return (
-        <div className="navbar bg-base-100 shadow-sm">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg aria-label="Menu" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+        <nav className=' container mx-auto'>
+            <div className="navbar bg-base-100 shadow-sm">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg aria-label="Menu" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex={-1}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            {links}
+                        </ul>
                     </div>
-                    <ul
-                        tabIndex={-1}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                    <Link href="/" className="btn btn-ghost text-xl flex items-center gap-2">
+                        <Image src="/logo.png" alt="FitLog logo" width={28} height={28} />
+                        FITLOG
+                    </Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 rounded-6xl">
                         {links}
                     </ul>
                 </div>
-                <Link href="/" className="btn btn-ghost text-xl flex items-center gap-2">
-                    <Image src="/logo.png" alt="FitLog logo" width={28} height={28} />
-                    FITLOG
-                </Link>
+                <div className="navbar-end font-white font-serif">
+                    <a className="btn">Plan</a>
+                    <a className="btn">Saved </a>
+                </div>
             </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1 rounded-6xl">
-                    {links}
-                </ul>
-            </div>
-            <div className="navbar-end font-white font-serif">
-                <a className="btn">Plan</a>
-                <a className="btn">Saved </a>
-            </div>
-        </div>
+        </nav>
     );
 };
 
