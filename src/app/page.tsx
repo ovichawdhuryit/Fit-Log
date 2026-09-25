b1e3b4fb9112e2216353de37e8f7618ef2d5ead7
Anik Chawdhury <ovichawdhuryit@gmail.com>
2026-09-25 14:51:41 +0600

    Initial Navbar Boiler Plate structured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import NavbarPage from "./Navbar/page";
+
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <NavbarPage></NavbarPage>
+    <Navbar></Navbar>
     </div>
   );
 }
